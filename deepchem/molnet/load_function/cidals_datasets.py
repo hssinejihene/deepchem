6b87beb10cf7f95dc27fdde42c7d59211bad0ff1
hssinejihene <hssinejihen@gmail.com>
2020-08-08 12:11:04 +0100

    Update cidals_datasets.py

--- deepchem/molnet/load_function/cidals_datasets.py
+++ deepchem/molnet/load_function/cidals_datasets.py
@@ -8,7 +8,7 @@ import deepchem
 logger = logging.getLogger(__name__)
 
 DEFAULT_DIR = deepchem.utils.get_data_dir()
-cidals_URL = "https://drive.google.com/file/d/1AyyJL5h-OV_eG_UZymfcBEStbcMVEhda/view"
+cidals_URL ="cidals.csv"
 
 
 def load_cidals(featurizer='ECFP',
